73fee5e8ff9fb23f68eb4376da0a7256d2e621b0
PorKunut <87304275+PorKunut@users.noreply.github.com>
2021-07-12 19:22:38 +0700

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+  public static void main (String[] args) {
+     int i = 0;
+     for (i = 1; i <= 100; i++)
+        if (i % 12 == 0)
+        System.out.println("จำนวนเต็มที่หาร 12 ลงตัว = " + i);
+  }
+
+}
